cb64e02b725a4b2b8c70cef2607e109e448e77f6
threefireisYan <48510432+threefireisYan@users.noreply.github.com>
2023-05-06 16:24:59 +0800

    第一次运行gin框架

--- gin-test/main.go
+++ gin-test/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
-	gin.Default()
+	testhttps := gin.Default()
+	//crul http://localhost:8080/hello  GET 获取到 json 返回值 {"name":"hello world"}
+	testhttps.GET("/hello", func(ctx *gin.Context) {
+		//可返回数据 数组 map list struct机构提
+		ctx.JSON(200, gin.H{
+			"name": "hello world",
+		})
+	})
+	//启动服务  8080前面的冒号不能省略
+	err := testhttps.Run(":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
